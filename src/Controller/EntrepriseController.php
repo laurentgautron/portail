55addc8cab3706a1ba90780393996e0663411d10
laurentgautron <laurentgautron@protonmail.com>
2021-08-28 12:34:27 +0200

    feature: modification et suppression d'une entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Entreprise;
 use App\Form\EntrepriseType;
+use App\Repository\EntrepriseRepository;
+use App\Repository\ExperienceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +15,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class EntrepriseController extends AbstractController
 {
     /**
-     * @Route("/entreprise", name="entreprise")
+     * @Route("/entreprise/show", name="app_show_entreprise")
      */
-    public function index(): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
-        return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
-        ]);
+        $entreprises = $entrepriseRepository->findAll();
+
+        return $this->render('entreprise/show.html.twig', compact('entreprises'));
     }
 
     /**
@@ -44,6 +46,44 @@ class EntrepriseController extends AbstractController
 
         return $this->render('entreprise/add.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/entreprise/delete/{id<[0-9]+>}", name="app_delete_entreprise")
+     */
+    public function delete(EntityManagerInterface $em, Entreprise $entreprise, EntrepriseRepository $entrepriseRepository, ExperienceRepository $experienceRepository)
+    {
+        $entrepriseId = $entrepriseRepository->findBy(['id' => $entreprise->getId()])[0]->getId();
+        //dd($entrepriseId);
+        $experience = $experienceRepository->findBy(['entreprise' => $entrepriseId])[0];
+        //dd($experience);
+        $experience->setEntreprise(null);
+        $em->persist($experience);
+        $em->remove($entreprise);
+        $em->flush();
+        return $this->redirectToRoute('app_show_entreprise');
+    }
+
+    /**
+     * @Route("/entreprise/modification/{id<[0-9]+>}", name="app_modification_entreprise")
+     */
+    public function modify(Request $request, Entreprise $entreprise, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() and $form->isValid()) {
+            $em->persist($entreprise);
+            $em->flush();
+
+            return $this->redirectToRoute('app_show_entreprise');
+        }
+
+        return $this->render('entreprise/modify.html.twig',[
+            'form' => $form->createView(),
+            'entreprise' => $entreprise
         ]);
     }
 }
